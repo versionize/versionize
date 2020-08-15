c6ac77aa3b5c1c814f6f81edb24fd8f0e8452f73
saintedlama <Christoph.Walcher@gmail.com>
2020-08-15 10:28:41 +0200

    chore: add more tests

---
+++ Versionize.Tests/ProjectTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using Shouldly;
+using Versionize.Tests.TestSupport;
+using Xunit;
+
+namespace Versionize.Tests
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ShouldThrowInCaseOfInvalidVersion()
+        {
+            var tempDir = TempDir.Create();
+            var projectFileContents = @"<Project Sdk=""Microsoft.NET.Sdk"">
+    <PropertyGroup>
+        <Version>abcd</Version>
+    </PropertyGroup>
+</Project>";
+
+            var projectFilePath = Path.Join(tempDir, "test.csproj");
+            File.WriteAllText(projectFilePath, projectFileContents);
+
+            Should.Throw<InvalidOperationException>(() => Project.Create(projectFilePath));
+        }
+
+        [Fact]
+        public void ShouldThrowInCaseOfInvalidXml()
+        {
+            var tempDir = TempDir.Create();
+            var projectFileContents = @"<Project Sdk=""Microsoft.NET.Sdk"">
+    <PropertyGroup>
+        <Version>1.0.0</Version>
+    </PropertyGroup>
+";
+
+            var projectFilePath = Path.Join(tempDir, "test.csproj");
+            File.WriteAllText(projectFilePath, projectFileContents);
+
+            Should.Throw<InvalidOperationException>(() => Project.Create(projectFilePath));
+        }
+        
+        [Fact]
+        public void ShouldUpdateTheVersionElementOnly()
+        {
+            var tempDir = TempDir.Create();
+            var projectFileContents = 
+                @"<Project Sdk=""Microsoft.NET.Sdk"">
+    <PropertyGroup>
+        <Version>1.0.0</Version>
+    </PropertyGroup>
+</Project>";
+
+            var projectFilePath = Path.Join(tempDir, "test.csproj");
+            File.WriteAllText(projectFilePath, projectFileContents);
+
+            var project = Project.Create(projectFilePath);
+            project.WriteVersion(new Version(2, 0, 0));
+
+            var versionedProjectContents = File.ReadAllText(projectFilePath);
+            
+            versionedProjectContents.ShouldBe(projectFileContents.Replace("1.0.0", "2.0.0"));
+        }
+    }
+}
--- Versionize.Tests/WorkingCopyTests.cs
+++ Versionize.Tests/WorkingCopyTests.cs
@@ -6,22 +6,12 @@ using Versionize.Tests.TestSupport;
 using Versionize.CommandLine;
 using LibGit2Sharp;
 using System.Xml;
-using System.Linq;
 using System.Diagnostics;
 
 namespace Versionize.Tests
 {
     public class WorkingCopyTests
     {
-        // Thanks @dtb https://stackoverflow.com/a/1344242
-        private static Random _random = new Random();
-        private static string _RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[_random.Next(s.Length)]).ToArray());
-        }
-
         public WorkingCopyTests()
         {
             CommandLineUI.Platform = new TestPlatformAbstractions();
@@ -38,7 +28,8 @@ namespace Versionize.Tests
         [Fact]
         public void ShouldExitIfNoWorkingCopyCouldBeDiscovered()
         {
-            var directoryWithoutWorkingCopy = Path.Combine(Path.GetTempPath(), "ShouldExitIfNoWorkingCopyCouldBeDiscovered");
+            var directoryWithoutWorkingCopy =
+                Path.Combine(Path.GetTempPath(), "ShouldExitIfNoWorkingCopyCouldBeDiscovered");
             Directory.CreateDirectory(directoryWithoutWorkingCopy);
 
             Assert.Throws<CommandLineExitException>(() => WorkingCopy.Discover(directoryWithoutWorkingCopy));
@@ -64,72 +55,37 @@ namespace Versionize.Tests
         [Fact]
         public void ShouldIgnoreInsignificantCommits()
         {
-            // Set directory and filenames
-            var random = new Random();
-            var tempDir = $"{Path.GetTempPath()}{_RandomString(10)}";
-            var tempFile = $"{tempDir}/{_RandomString(10)}.txt";
-            var csProjFile = $"{tempDir}/{Path.GetFileName(tempDir)}.csproj";
-            Directory.CreateDirectory(tempDir);
-            Repository.Init(tempDir);
-
-            // Make sure we have a git author or versionize will fail to make a commit
-            var gitConfigCmd = new ProcessStartInfo("git", "config user.name VersionizeTest");
-            gitConfigCmd.WorkingDirectory = tempDir;
-            Process.Start(gitConfigCmd).WaitForExit();
-            gitConfigCmd.Arguments = "config user.email noreply@versionize.com";
-            Process.Start(gitConfigCmd).WaitForExit();
-
-            // Initialize git repo
-            var repo = new Repository(tempDir);
-
-            // Create .net project
-            Process.Start("dotnet", $"new console -o {tempDir}").WaitForExit();
-
-            // Add version string to csproj
-            XmlDocument doc = new XmlDocument();
-            doc.PreserveWhitespace = true;
-
-            try
-            {
-                doc.Load(csProjFile);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-            var projectNode = doc.SelectSingleNode("/Project/PropertyGroup");
-            var versionNode = doc.CreateNode("element", "Version", "");
-            versionNode.InnerText = "0.0.0";
-            projectNode.AppendChild(versionNode);
-            using (var tw = new XmlTextWriter(csProjFile, null))
-            {
-                doc.Save(tw);
-            }
+            var workingDirectory = TempDir.Create();
+            using var tempRepository = TempRepository.Create(workingDirectory);
+            
+            TempCsProject.Create(tempRepository.Info.WorkingDirectory);
 
             // Create author
             var author = new Signature("Gitty McGitface", "noreply@git.com", DateTime.Now);
 
+            var workingFilePath = Path.Join(workingDirectory, "hello.txt");
+
             // Create and commit testfile
-            File.WriteAllText(tempFile, "First line of text");
-            Commands.Stage(repo, "*");
-            repo.Commit("feat: Initial commit", author, author);
+            File.WriteAllText(workingFilePath, "First line of text");
+            Commands.Stage(tempRepository, "*");
+            tempRepository.Commit("feat: Initial commit", author, author);
 
             // Run versionize
-            var workingCopy = WorkingCopy.Discover(tempDir);
+            var workingCopy = WorkingCopy.Discover(workingDirectory);
             workingCopy.Versionize();
 
             // Add insignificant change
-            using (var sw = File.AppendText(tempFile))
+            using (var sw = File.AppendText(workingFilePath))
             {
                 sw.WriteLine("This is another line of text");
             }
-            Commands.Stage(repo, "*");
+
+            Commands.Stage(tempRepository, "*");
             author = new Signature("Gitty McGitface", "noreply@git.com", DateTime.Now);
-            repo.Commit("chore: Added line of text", author, author);
+            tempRepository.Commit("chore: Added line of text", author, author);
 
             // Get last commit
-            var lastCommit = repo.Head.Tip;
+            var lastCommit = tempRepository.Head.Tip;
 
             // Run versionize, ignoring insignificant commits
             try
@@ -140,20 +96,11 @@ namespace Versionize.Tests
             {
                 Assert.Equal(0, ex.ExitCode);
             }
-            Assert.Equal(lastCommit, repo.Head.Tip);
-            repo.Dispose();
+
+            Assert.Equal(lastCommit, tempRepository.Head.Tip);
 
             // Cleanup
-            // Need to cleanup readonly attributes first...
-            foreach (var file in Directory.GetFiles(tempDir, "*", SearchOption.AllDirectories))
-            {
-                var attribs = File.GetAttributes(file);
-                if (attribs.HasFlag(FileAttributes.ReadOnly))
-                {
-                    File.SetAttributes(file, attribs & ~FileAttributes.ReadOnly);
-                }
-            }
-            Directory.Delete(tempDir, true);
+            Cleanup.DeleteDirectory(workingDirectory);
         }
     }
 }
