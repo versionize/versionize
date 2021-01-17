5ffca1a97475a9d7a9ff2763705d02cec680067f
Christoph Walcher <Christoph.Walcher@gmail.com>
2021-01-17 10:13:09 +0100

    feat: add azure links (#22)
    * Feat(Test): adding text azure to the projecte solution
    
    * Feat(Test): renaming test
    
    * Feat(Test): renaming method test
    
    * Feat(Test): wrong name changelog link builder
    
    beside have it the azure link build still have it the github link builder
    
    Co-authored-by: Enver Francisco <enver.francisco@gmail.com>

---
+++ Versionize/AzureLinkBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Versionize;
+
+namespace Versionize
+{
+    public class AzureLinkBuilder: IChangelogLinkBuilder
+    {
+        private readonly string _organization;
+        private readonly string _repository;
+
+        public AzureLinkBuilder(string pushUrl)
+        {
+            if (pushUrl.StartsWith("git@ssh.dev.azure.com:"))
+            {
+                var httpsPattern = new Regex("^git@ssh.dev.azure.com:(?<organization>.*?)/(?<repository>.*?)\\.git$");
+                var matches = httpsPattern.Match(pushUrl);
+
+                if (!matches.Success)
+                {
+                    throw new InvalidOperationException($"Remote url {pushUrl} is not recognized as valid Azure SSH pattern");
+                }
+
+                _organization = matches.Groups["organization"].Value;
+                _repository = matches.Groups["repository"].Value;
+            }
+            else if (pushUrl.StartsWith("https://") && pushUrl.Contains("@dev.azure.com/"))
+            {
+                var httpsPattern = new Regex("^https://(?<organization>.*?)@dev.azure.com/(?<organization>.*?)/(?<repository>.*?)\\.git$");
+                var matches = httpsPattern.Match(pushUrl);
+
+                if (!matches.Success)
+                {
+                    throw new InvalidOperationException($"Remote url {pushUrl} is not recognized as valid Azure HTTPS pattern");
+                }
+                _organization = matches.Groups["organization"].Value;
+                _repository = matches.Groups["repository"].Value;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Remote url {pushUrl} is not recognized as Azure SSH or HTTPS url");
+            }
+        }
+
+        public string BuildVersionTagLink(Version version)
+        {
+            return $"https://{_organization}@dev.azure.com/{_organization}/{_repository}/releases/tag/v{version}";
+        }
+
+        public string BuildCommitLink(ConventionalCommit commit)
+        {
+            return $"https://{_organization}@dev.azure.com/{_organization}/{_repository}/commit/{commit.Sha}";
+        }
+    }
+}
