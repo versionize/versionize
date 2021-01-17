c325b65514cabc2ba8d2ee0269009092ed0f0b32
saintedlama <christoph.walcher@gmail.com>
2021-01-17 10:27:59 +0100

    chore: add tests for https and ssh commit links

--- Versionize.Tests/RemotesLinkBuilderAzureTests.cs
+++ Versionize.Tests/RemotesLinkBuilderAzureTests.cs
@@ -44,7 +44,33 @@ namespace Versionize.Tests
             linkBuilder.ShouldBeAssignableTo<PlainLinkBuilder>();
         }
 
-        private Repository SetupRepositoryWithRemote(string remoteName, string pushUrl)
+        [Fact]
+        public void ShouldBuildASSHCommitLink()
+        {
+            var commit = new ConventionalCommit {
+                Sha = "734713bc047d87bf7eac9674765ae793478c50d3"
+            };
+
+            var linkBuilder = new AzureLinkBuilder("git@ssh.dev.azure.com:v3/dosse/DosSE.ERP.Cloud/ERP.git");
+            var link = linkBuilder.BuildCommitLink(commit);
+
+            link.ShouldBe("https://v3@dev.azure.com/v3/dosse/DosSE.ERP.Cloud/ERP/commit/734713bc047d87bf7eac9674765ae793478c50d3");
+        }
+
+        [Fact]
+        public void ShouldBuildAHTTPSCommitLink()
+        {
+            var commit = new ConventionalCommit {
+                Sha = "734713bc047d87bf7eac9674765ae793478c50d3"
+            };
+
+            var linkBuilder = new AzureLinkBuilder("https://dosse@dev.azure.com/dosse/DosSE.ERP.Cloud/_git/ERP.git");
+            var link = linkBuilder.BuildCommitLink(commit);
+
+            link.ShouldBe("https://dosse@dev.azure.com/dosse/DosSE.ERP.Cloud/_git/ERP/commit/734713bc047d87bf7eac9674765ae793478c50d3");
+        }
+
+        private static Repository SetupRepositoryWithRemote(string remoteName, string pushUrl)
         {
             var workingDirectory = TempDir.Create();
             var repo = TempRepository.Create(workingDirectory);
