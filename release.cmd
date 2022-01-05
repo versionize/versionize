ce749b8d4acacbfa4ecf3a4e6b63de8669a21cdc
Christoph Walcher <Christoph.Walcher@gmail.com>
2022-01-05 09:34:29 +0100

    chore: extend the release script including better error handling

--- release.cmd
+++ release.cmd
@@ -1,5 +1,7 @@
 @echo off
+git checkout master
+git pull || echo Could not pull changes from origin && exit /b
 
-dotnet run --project Versionize --framework net5.0
-rm -rf nupkgs
-dotnet pack --output nupkgs --include-source --configuration Release --include-symbols
+dotnet run --project Versionize --framework net6.0 || echo Exiting. Could not create a new version using versionize && exit /b
+rm -rf nupkgs || echo ERROR && exit /b
+dotnet pack --output nupkgs --include-source --configuration Release --include-symbols || echo Exiting. Could not run dotnet pack. Remove any tags created by versionize && exit /b
