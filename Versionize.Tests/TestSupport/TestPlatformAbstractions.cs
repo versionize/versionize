913f253d4eb61c7c0ebe4b5cd60b742a3ce57aef
Christoph Walcher <Christoph.Walcher@gmail.com>
2022-02-19 06:46:07 +0100

    chore: add test and test simplifications

--- Versionize.Tests/TestSupport/TestPlatformAbstractions.cs
+++ Versionize.Tests/TestSupport/TestPlatformAbstractions.cs
@@ -5,7 +5,7 @@ namespace Versionize.Tests.TestSupport;
 public class TestPlatformAbstractions : IPlatformAbstractions
 {
     public LogLevel Verbosity { get; set; }
-    public List<List<string>> Messages { get; } = new List<List<string>>();
+    public List<string> Messages { get; } = new List<string>();
 
     public void Exit(int exitCode)
     {
@@ -14,11 +14,11 @@ public class TestPlatformAbstractions : IPlatformAbstractions
 
     public void WriteLine(string message, ConsoleColor color)
     {
-        Messages.Add(new List<string> { message });
+        Messages.Add(message);
     }
 
     public void WriteLine(params (string text, ConsoleColor color)[] messages)
     {
-        Messages.Add(messages.Select(m => m.text).ToList());
+        Messages.Add(string.Concat(messages.Select(m => m.text)));
     }
 }
