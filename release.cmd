59e56731567b3a2e387f649478ab61fd616d1d73
Christoph Walcher <Christoph.Walcher@gmail.com>
2022-02-27 10:22:57 +0100

    chore: include git push in release cmd

--- release.cmd
+++ release.cmd
@@ -4,6 +4,8 @@ git checkout master
 git pull || echo Could not pull changes from origin && exit /b
 
 dotnet run --project Versionize --framework net6.0 || echo Exiting. Could not create a new version using versionize && exit /b
+git push --follow-tags origin master  || echo Exiting. Could push changes
+
 rm -rf nupkgs || echo ERROR && exit /b
 dotnet pack --output nupkgs --include-source --configuration Release --include-symbols || echo Exiting. Could not run dotnet pack. Remove any tags created by versionize && exit /b
 
