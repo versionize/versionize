2858d813ff5103f10900c3f5cdc976a8e621fd02
Christoph Walcher <Christoph.Walcher@gmail.com>
2022-03-03 05:17:13 +0100

    chore: correct error message and exit

--- release.cmd
+++ release.cmd
@@ -4,7 +4,7 @@ git checkout master
 git pull || echo Could not pull changes from origin && exit /b
 
 dotnet run --project Versionize --framework net6.0 || echo Exiting. Could not create a new version using versionize && exit /b
-git push --follow-tags origin master  || echo Exiting. Could push changes
+git push --follow-tags origin master || echo Exiting. Failed to push changes to repository && exit /b
 
 rm -rf nupkgs || echo ERROR && exit /b
 dotnet pack --output nupkgs --include-source --configuration Release --include-symbols || echo Exiting. Could not run dotnet pack. Remove any tags created by versionize && exit /b
