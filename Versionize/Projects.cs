08c800f28cc3924baa4765793d65351a8834e63e
Anto Subash <antosubash@live.com>
2022-10-20 16:47:19 +0200

    feat: added "*.props" to the filter to version the props file

--- Versionize/Projects.cs
+++ Versionize/Projects.cs
@@ -32,7 +32,7 @@ public class Projects
 
     public static Projects Discover(string workingDirectory)
     {
-        var filters = new[] { "*.vbproj", "*.csproj", "*.fsproj" };
+        var filters = new[] { "*.vbproj", "*.csproj", "*.fsproj", "*.props" };
 
         var options = new EnumerationOptions
         {
